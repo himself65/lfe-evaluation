5722771927d5b9bf856ff862bdc2bf3859321827
himself65 <himself6565@gmail.com>
2019-01-02 22:53:55 +0800

    INIT REPO

---
+++ jest.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  verbose: false,
+  roots: [
+    '<rootDir>/src',
+    '<rootDir>/tests/unit'
+  ],
+  moduleFileExtensions: [
+    "js",
+    "json"
+  ],
+  moduleDirectories: ['node_modules'],
+  moduleNameMapper: {
+    '^@/test$': '<rootDir>/test/index.js',
+    '^@/test/(.*)$': '<rootDir>/test/$1',
+    '^src/(.*)$': '<rootDir>/src/$1'
+  },
+  transform: {
+    "\\.(js)$": "babel-jest"
+  },
+  collectCoverageFrom: [
+    'app.js',
+    "src/**/*.{js}",
+    "!**/node_modules/**"
+  ],
+  transformIgnorePatterns: [
+    'node_modules'
+  ],
+  coverageDirectory: "./coverage/"
+}
